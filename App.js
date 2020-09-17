255d58585ecc3739eb9b4d4ba2801aa3cec3bf48
wangpradit01 <s6135512036@phuket.psu.ac.th>
2020-09-17 11:26:22 +0700

    commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import Weather from './components/Weather';
+
+export default function App() {
+
+  return (
+    <View style={styles.container}>
+      <Weather zipCode="83120" />
+      <StatusBar style="auto" />
+    </View>
+  );
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    //alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
---
+++ components/Weather.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+import Forecast from './Forecast';
+
+export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+        main: 'main',
+        description: 'description',
+        temp: 0,
+    })
+    return (
+        <View>
+            <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+                <Text>Zip Code</Text>
+                <Text>{props.zipCode}</Text>
+                <Forecast {...forecastInfo} />
+            </ImageBackground>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    backdrop: {
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+        
+    },
+});
